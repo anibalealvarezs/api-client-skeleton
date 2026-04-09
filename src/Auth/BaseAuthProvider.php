f475e5342f4745d2a903d697092750b83fb90589
Anibal Alvarez <aalvarez@chmw.co>
2026-04-09 10:05:10 -0400

    fix: restore BaseAuthProvider with multi-type constructor support

--- src/Auth/BaseAuthProvider.php
+++ src/Auth/BaseAuthProvider.php
@@ -8,19 +8,24 @@ use Anibalealvarezs\ApiSkeleton\Interfaces\AuthProviderInterface;
 
 abstract class BaseAuthProvider implements AuthProviderInterface
 {
-    protected string $tokenPath;
     protected array $data = [];
+    protected string $filePath = "";
 
-    public function __construct(string $tokenPath = "")
+    public function __construct(array|string $configOrPath = "")
     {
-        $this->tokenPath = $tokenPath;
+        if (is_array($configOrPath)) {
+            $this->data = $configOrPath;
+            return;
+        }
+
+        $this->filePath = $configOrPath;
         $this->load();
     }
 
     protected function load(): void
     {
-        if ($this->tokenPath && file_exists($this->tokenPath)) {
-            $content = file_get_contents($this->tokenPath);
+        if ($this->filePath && file_exists($this->filePath)) {
+            $content = file_get_contents($this->filePath);
             if ($content) {
                 $this->data = json_decode($content, true) ?: [];
             }
@@ -29,12 +34,12 @@ abstract class BaseAuthProvider implements AuthProviderInterface
 
     protected function save(): void
     {
-        if ($this->tokenPath) {
-            $dir = dirname($this->tokenPath);
+        if ($this->filePath) {
+            $dir = dirname($this->filePath);
             if (!is_dir($dir)) {
                 mkdir($dir, 0755, true);
             }
-            file_put_contents($this->tokenPath, json_encode($this->data, JSON_PRETTY_PRINT));
+            file_put_contents($this->filePath, json_encode($this->data, JSON_PRETTY_PRINT));
         }
     }
 
